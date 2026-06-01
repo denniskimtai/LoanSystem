da6e4344a61a054358dd3282f8bbe264a077f8a3
Dennis Too (Techno Brain Global FZ LLC) <v-dennistoo@microsoft.com>
2026-06-01 11:28:51 +0300

    fix(api): configure json to deserialize enums from strings to fix 400 bad request on interactions

--- LoanSystem.Api/Program.cs
+++ LoanSystem.Api/Program.cs
@@ -27,7 +27,11 @@ Log.Logger = new LoggerConfiguration()
 builder.Host.UseSerilog();
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
 builder.Services.AddCors(options =>
